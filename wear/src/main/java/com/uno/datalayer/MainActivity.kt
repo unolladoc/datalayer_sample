054562705d70b1578b4415a1bc1b2ebd739bef6c
uno <rubenlladoc@gmail.com>
2022-07-21 14:40:32 +0800

    check nodes

--- wear/src/main/java/com/uno/datalayer/MainActivity.kt
+++ wear/src/main/java/com/uno/datalayer/MainActivity.kt
@@ -2,11 +2,18 @@ package com.uno.datalayer
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.wearable.CapabilityClient
+import com.google.android.gms.wearable.Node
 import com.google.android.gms.wearable.Wearable
 import com.uno.datalayer.databinding.ActivityMainBinding
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class MainActivity : ComponentActivity() {
 
@@ -18,12 +25,103 @@ class MainActivity : ComponentActivity() {
 
     private val clientDataViewModel by viewModels<ClientDataViewModel>()
 
+    private var transcriptionNodeId: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.hear.setOnClickListener {
+            getNodeId()
+        }
+
+    }
+
+    private fun getNodeId() {
+        lifecycleScope.launch {
+            try {
+                val nodes = getCapabilitiesForReachableNodes()
+                    .filterValues { "voice_transcription" in it}.keys
+                updateNodeId(nodes)
+            } catch (cancellationException: CancellationException) {
+                throw cancellationException
+            } catch (exception: Exception) {
+                Log.d(TAG, "Querying nodes failed: $exception")
+            }
+        }
+    }
+
+    private fun updateNodeId(nodes: Set<Node>) {
+        val message: String?
+
+        if (nodes.isEmpty()) {
+            message = getString(R.string.no_device)
+        } else {
+            message = getString(R.string.connected_nodes, nodes.joinToString(", ") { it.displayName })
+            transcriptionNodeId = nodes.firstOrNull { it.isNearby }?.id ?: nodes.firstOrNull()?.id
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun requestTranscription(voiceData: ByteArray) {
+        transcriptionNodeId?.also { nodeId ->
+            lifecycleScope.launch {
+                try {
+                    messageClient.sendMessage(
+                        nodeId,
+                        VOICE_TRANSCRIPTION_MESSAGE_PATH,
+                        voiceData
+                    ).await()
+                    Log.d(TAG, "Voice data sent")
+                } catch (cancellationException: CancellationException) {
+                    throw cancellationException
+                } catch (exception: Exception) {
+                    Log.d(TAG, "FAILED to send voice data")
+                }
+            }
+        }
+    }
+
+    private fun checkNodes() {
+        lifecycleScope.launch {
+            try {
+                val nodes = getCapabilitiesForReachableNodes()
+                    .filterValues { "mobile" in it || "wear" in it }.keys
+                displayNodes(nodes)
+            } catch (cancellationException: CancellationException) {
+                throw cancellationException
+            } catch (exception: Exception) {
+                Log.d(TAG, "Querying nodes failed: $exception")
+            }
+        }
+    }
+
+    private suspend fun getCapabilitiesForReachableNodes(): Map<Node, Set<String>> =
+        capabilityClient.getAllCapabilities(CapabilityClient.FILTER_REACHABLE)
+            .await()
+            // Pair the list of all reachable nodes with their capabilities
+            .flatMap { (capability, capabilityInfo) ->
+                capabilityInfo.nodes.map { it to capability }
+            }
+            // Group the pairs by the nodes
+            .groupBy(
+                keySelector = { it.first },
+                valueTransform = { it.second }
+            )
+            // Transform the capability list for each node into a set
+            .mapValues { it.value.toSet() }
+
+    private fun displayNodes(nodes: Set<Node>) {
+        val message = if (nodes.isEmpty()) {
+            getString(R.string.no_device)
+        } else {
+            getString(R.string.connected_nodes, nodes.joinToString(", ") { it.displayName })
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
@@ -46,5 +144,7 @@ class MainActivity : ComponentActivity() {
 
     companion object {
         private const val TAG = "MainActivity"
+        const val VOICE_TRANSCRIPTION_MESSAGE_PATH = "/voice_transcription"
+        private const val MODEL_FILE = "yamnet.tflite"
     }
 }
